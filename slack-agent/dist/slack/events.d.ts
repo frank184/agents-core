5f09eaff9df2c74e6d9a77005ebeef7047485804
Francois Belanger <fbelanger@integnations.com>
2026-03-06 03:19:46 -0500

    chore: Setup husky pre-commit hooks for commit linting

--- slack-agent/dist/slack/events.d.ts
+++
@@ -1,4 +0,0 @@
-import { App } from '@slack/bolt';
-import { SlackClient } from './client';
-export declare function registerEventHandlers(app: App, slackClient: SlackClient): void;
-//# sourceMappingURL=events.d.ts.map
